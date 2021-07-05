1f119cb395b85a0120191d5344741f326f960184
pedrohrf <pedro.freitas@yandeh.com.br>
2021-07-05 00:48:15 -0300

    criação dos diretorios necessarios

--- geralda.py
+++ geralda.py
@@ -2,16 +2,22 @@ import discord
 from src.on_message import execute
 from src.helpers.discord_response import response
 from src.enums import system_variables as sv
+import os
 
 client = discord.Client()
 
 
-async def invalid_cmd(message):
-    print(f"Comando Invalido {message}")
+async def configure():
+    dirs = ["./csv", "./graphs", "./saved"]
+    mode = 0o666
+    for d in dirs:
+        if not os.path.exists(d):
+            os.mkdir(d, mode)
 
 
 @client.event
 async def on_ready():
+    await configure()
     print(f'{client.user} has connected to Discord!')
 
 
